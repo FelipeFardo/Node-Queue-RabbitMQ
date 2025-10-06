18ae9e5ccaf7e82300fd8a0fa7a5601e845a4e3b
Felipe Fardo <felipe.fardo@hotmail.com.br>
2025-10-06 02:25:34 -0300

    fix: publisher wait for confirms

--- src/publisher.ts
+++ src/publisher.ts
@@ -5,18 +5,18 @@ const EXCHANGE_NAME = "exchange";
 
 const sendMsg = async () => {
 	const connection = await amqp.connect(env.RABBITMQ_URL);
-	const channel = await connection.createChannel();
+  const channel = await connection.createConfirmChannel();
 
 	const msg = { id: Date.now(), texto: "Hello world" };
 
-	// await channel.assertExchange(EXCHANGE_NAME, "direct", { durable: true });
-
-	channel.publish(EXCHANGE_NAME, "123", Buffer.from(JSON.stringify(msg)));
+	channel.publish(EXCHANGE_NAME, "route", Buffer.from(JSON.stringify(msg)),{ persistent: true });
 
 	console.log("Mensagem enviada:", msg);
 
-	connection.close();
-	process.exit(0);
+	await channel.waitForConfirms();
+	
+  connection.close();
+  process.exit(0);
 };
 
 sendMsg();
