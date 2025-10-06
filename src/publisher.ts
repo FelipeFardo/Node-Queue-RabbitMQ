84cfdf68590273fa9723b1a2c65df249b4f15adb
Felipe Fardo <felipe.fardo@hotmail.com.br>
2025-10-06 02:33:36 -0300

    lint: format with biome

--- src/publisher.ts
+++ src/publisher.ts
@@ -5,18 +5,20 @@ const EXCHANGE_NAME = "exchange";
 
 const sendMsg = async () => {
 	const connection = await amqp.connect(env.RABBITMQ_URL);
-  const channel = await connection.createConfirmChannel();
+	const channel = await connection.createConfirmChannel();
 
 	const msg = { id: Date.now(), texto: "Hello world" };
 
-	channel.publish(EXCHANGE_NAME, "route", Buffer.from(JSON.stringify(msg)),{ persistent: true });
+	channel.publish(EXCHANGE_NAME, "route", Buffer.from(JSON.stringify(msg)), {
+		persistent: true,
+	});
 
 	console.log("Mensagem enviada:", msg);
 
 	await channel.waitForConfirms();
-	
-  connection.close();
-  process.exit(0);
+
+	connection.close();
+	process.exit(0);
 };
 
 sendMsg();
